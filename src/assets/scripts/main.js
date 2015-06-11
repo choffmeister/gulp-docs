300d2012c143d9f8f8b3f742895a8f1b95914ec3
Christian Hoffmeister <mail@choffmeister.de>
2015-06-11 21:34:25 +0200

    Fix navbar

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,5 @@
-var React = require('react');
+/*eslint-env node*/
+/*eslint-env browser*/
+window.jQuery = require('jquery');
+window.Bootstrap = require('bootstrap');
+window.React = require('react');
